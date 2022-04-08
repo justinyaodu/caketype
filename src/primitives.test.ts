ba9e9a98e9073ff473d7becd111616e7e7fd8ff5
Justin Yao Du <justinyaodu@gmail.com>
2022-04-07 18:20:38 -0700

    Initial commit

---
+++ src/primitives.test.ts
@@ -0,0 +1,65 @@
+import { isPrimitive, reprPrimitive } from "./primitives";
+
+test("bigint is primitive", () => {
+  expect(isPrimitive(BigInt(200))).toBe(true);
+});
+
+test("boolean is primitive", () => {
+  expect(isPrimitive(true)).toBe(true);
+});
+
+test("number is primitive", () => {
+  expect(isPrimitive(3.14)).toBe(true);
+});
+
+test("string is primitive", () => {
+  expect(isPrimitive("apple")).toBe(true);
+});
+
+test("symbol is primitive", () => {
+  expect(isPrimitive(Symbol.iterator)).toBe(true);
+});
+
+test("null is primitive", () => {
+  expect(isPrimitive(null)).toBe(true);
+});
+
+test("array is not primitive", () => {
+  expect(isPrimitive([])).toBe(false);
+});
+
+test("object is not primitive", () => {
+  expect(isPrimitive({})).toBe(false);
+});
+
+test("undefined is not primitive", () => {
+  expect(isPrimitive(undefined)).toBe(false);
+});
+
+test("repr bigint", () => {
+  expect(reprPrimitive(BigInt(-27))).toEqual("-27n");
+});
+
+test("repr boolean", () => {
+  expect(reprPrimitive(false)).toEqual("false");
+})
+
+test("repr number", () => {
+  expect(reprPrimitive(3.75)).toEqual("3.75");
+});
+
+test("repr -Infinity", () => {
+  expect(reprPrimitive(-Infinity)).toEqual("-Infinity");
+});
+
+test("repr string", () => {
+  expect(reprPrimitive("hi\n\t\"")).toEqual("\"hi\\n\\t\\\"\"");
+});
+
+test("repr symbol", () => {
+  expect(reprPrimitive(Symbol("foo"))).toEqual("Symbol(foo)");
+});
+
+test("repr null", () => {
+  expect(reprPrimitive(null)).toEqual("null");
+});
---
+++ src/primitives.ts
@@ -0,0 +1,35 @@
+type Primitive = bigint | boolean | number | string | symbol | null;
+
+function isPrimitive(input: unknown): input is Primitive {
+  switch (typeof input) {
+    case "bigint":
+    case "boolean":
+    case "number":
+    case "string":
+    case "symbol":
+      return true;
+    case "object":
+      return input === null;
+    default:
+      return false;
+  }
+}
+
+function reprPrimitive(value: Primitive): string {
+  switch (typeof value) {
+    case "bigint":
+      return value.toString() + "n";
+    case "string":
+      return JSON.stringify(value);
+    case "symbol":
+      return value.toString();
+    default:
+      return "" + value;
+  }
+}
+
+export {
+  Primitive,
+  isPrimitive,
+  reprPrimitive
+};
