e68fc9aa2c9ae21553af515dbf731eacedf1d89d
Justin Yao Du <justinyaodu@gmail.com>
2022-12-31 16:51:15 -0800

    Add eslint-plugin-tsdoc

--- jest-exactOptionalPropertyTypes.config.js
+++ jest-exactOptionalPropertyTypes.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable tsdoc/syntax */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   ...require("./jest.config"),
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable tsdoc/syntax */
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
