e429355722d161eb6707bb6d8761e1e9974fc49d
Justin Yao Du <justinyaodu@gmail.com>
2023-01-07 13:12:21 -0800

    Enforce 100% code coverage

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,12 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
